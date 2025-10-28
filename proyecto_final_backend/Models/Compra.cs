1dea5aa84f49f62969c6640016491c044bfd13d0
Totality123 <Steven2004medina@gmail.com>
2025-10-28 10:03:39 -0600

    1 compras y proveedores creados

---
+++ proyecto_final_backend/Models/Compra.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace proyecto_final_backend.Models
+{
+    [Table("compras")]
+    public class Compra
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey("IdProveedor")]
+        public required Proveedor Proveedor { get; set; }
+
+        [Required]
+        [Column("fecha")]
+        public DateOnly Fecha { get; set; }
+
+        [Required]
+        [Column("monto_total")]
+        public decimal MontoTotal { get; set; }
+
+        [Required]
+        [Column("deleted")]
+
+    }
+}
---
+++ proyecto_final_backend/Models/Proveedor.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyecto_final_backend.Models
+{
+    [Table("proveedores")]
+    public class Proveedor
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nombre")]
+        public required string Nombre { get; set; }
+
+        [Required]
+        [Column("nombre_contacto")]
+        public required string NombreContacto { get; set; }
+
+        [Required]
+        [Column("telefono")]
+        public required string Telefono { get; set; }
+
+        [Required]
+        [Column("correo")]
+        public required string Correo { get; set; }
+
+        [Required]
+        [Column("deleted")]
+        public bool Deleted { get; set; } = false;
+
+
+    }
+
+}
